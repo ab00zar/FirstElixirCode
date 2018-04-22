8e1892fe9de4c851c08433cd7f99ec4355dbfb23
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-04-22 23:47:56 +0200

    upcoming_matches method is added

--- lib/panda.ex
+++ lib/panda.ex
@@ -1,18 +1,10 @@
 defmodule Panda do
-  @moduledoc """
-  Documentation for Panda.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Panda.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def upcoming_matches do
+    url = "https://api.pandascore.co/matches/upcoming?sort=begin_at&page[size]=5&token=" <> Application.get_env(:panda, :token)
+    %{body: body} = HTTPoison.get! url
+    r = Poison.Parser.parse! body
+    for n <- r, do: %{"begin_at" => n["begin_at"], "id" => n["id"], "name" => n["name"]}
   end
+  
 end
