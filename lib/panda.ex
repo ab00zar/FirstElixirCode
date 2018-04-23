3436da5e93629636214acb2780411e6170617d34
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-04-23 16:20:24 +0200

    odds for match is added randomly

--- lib/panda.ex
+++ lib/panda.ex
@@ -7,4 +7,12 @@ defmodule Panda do
     for n <- r, do: %{"begin_at" => n["begin_at"], "id" => n["id"], "name" => n["name"]}
   end
   
+  def odds_for_match(match_id) do
+    url = "https://api.pandascore.co/matches/" <> Integer.to_string(match_id) <> "?&token=" <> Application.get_env(:panda, :token)
+    %{body: body} = HTTPoison.get! url
+    r = Poison.Parser.parse! body
+    rand = :rand.uniform * 100
+    %{List.first(r["opponents"])["opponent"]["name"] => rand, List.last(r["opponents"])["opponent"]["name"] => 100 - rand}
+  end
+  
 end
