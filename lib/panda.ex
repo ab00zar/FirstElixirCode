83353406e00a6523571bbbb8231d7cc00faf2fc6
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-06-03 21:58:29 +0200

    calculating odds based on matches each opponent won

--- lib/panda.ex
+++ lib/panda.ex
@@ -1,18 +1,35 @@
 defmodule Panda do
 
-  def upcoming_matches do
-    url = "https://api.pandascore.co/matches/upcoming?sort=begin_at&page[size]=5&token=" <> Application.get_env(:panda, :token)
-    %{body: body} = HTTPoison.get! url
-    r = Poison.Parser.parse! body
-    for n <- r, do: %{"begin_at" => n["begin_at"], "id" => n["id"], "name" => n["name"]}
-  end
+    def upcoming_matches do
+        url = "https://api.pandascore.co/matches/upcoming?sort=begin_at&page[size]=5&token=" <> Application.get_env(:panda, :token)
+        %{body: body} = HTTPoison.get! url
+        r = Poison.Parser.parse! body
+        for n <- r, do: %{"begin_at" => n["begin_at"], "id" => n["id"], "name" => n["name"]}
+    end
   
-  def odds_for_match(match_id) do
-    url = "https://api.pandascore.co/matches/" <> Integer.to_string(match_id) <> "?&token=" <> Application.get_env(:panda, :token)
-    %{body: body} = HTTPoison.get! url
-    r = Poison.Parser.parse! body
-    rand = :rand.uniform * 100
-    %{List.first(r["opponents"])["opponent"]["name"] => rand, List.last(r["opponents"])["opponent"]["name"] => 100 - rand}
-  end
+    def odds_for_match(match_id) do
+        url = "https://api.pandascore.co/matches/" <> Integer.to_string(match_id) <> "?&token=" <> Application.get_env(:panda, :token)
+        %{body: body} = HTTPoison.get!(url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
+        r = Poison.Parser.parse! body
+
+        #Number of matches opponent1 won
+        opp1_won_matches_url = "https://api.pandascore.co/matches/?filter[winner_id]=" <> Integer.to_string(List.first(r["opponents"])["id"]) 
+          <> "&token=" <> Application.get_env(:panda, :token)
+        %{body: body} = HTTPoison.get!(opp1_won_matches_url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
+        opp1_won_matches = length(Poison.Parser.parse! body)
+
+        #Number of matches opponent2 won
+        opp2_won_matches_url = "https://api.pandascore.co/matches/?filter[winner_id]=" <> Integer.to_string(List.last(r["opponents"])["id"]) 
+          <> "&token=" <> Application.get_env(:panda, :token)
+        %{body: body} = HTTPoison.get!(opp2_won_matches_url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
+        opp2_won_matches = length(Poison.Parser.parse! body)
+
+        #Calculate odds based on the number of matches each opponent won before
+        ratio = opp1_won_matches / opp2_won_matches
+        opp2_odds = 100 / (ratio + 1)
+        opp1_odds = 100 - opp2_odds
+
+        %{List.first(r["opponents"])["opponent"]["name"] => opp1_odds, List.last(r["opponents"])["opponent"]["name"] => opp2_odds}
+    end
   
 end
