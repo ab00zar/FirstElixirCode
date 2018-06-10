16362177dee6b8deaa1910e11a58f1306ee43de8
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-06-10 17:31:17 +0200

    httpoison problem is solved

--- lib/panda.ex
+++ lib/panda.ex
@@ -2,7 +2,7 @@ defmodule Panda do
 
     def upcoming_matches do
         url = "https://api.pandascore.co/matches/upcoming?sort=begin_at&page[size]=5&token=" <> Application.get_env(:panda, :token)
-        %{body: body} = HTTPoison.get! url
+        %{body: body} = HTTPoison.get!(url, [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
         r = Poison.Parser.parse! body
         for n <- r, do: %{"begin_at" => n["begin_at"], "id" => n["id"], "name" => n["name"]}
     end
